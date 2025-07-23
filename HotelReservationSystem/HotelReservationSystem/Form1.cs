7950576b403f092c0e64f24a2c63abb27046e6a7
Mark <markmanalo1717@gmail.com>
2025-07-22 17:06:16 -1200

    Finish UI layout

---
+++ HotelReservationSystem/HotelReservationSystem/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HotelReservationSystem.UserControls;
+
+namespace HotelReservationSystem
+{
+    public partial class ReservationSystem : Form
+    {
+        public ReservationSystem()
+        {
+            InitializeComponent();
+        }
+        private void LoadUserControl(UserControl control)
+        {
+            TopHomePanel.Controls.Clear();
+            control.Dock = DockStyle.Fill;
+            TopHomePanel.Controls.Add(control);
+        }
+
+        private void sataButton1_Click(object sender, EventArgs e) => LoadUserControl(new UCDashboard());
+        private void sataButton2_Click(object sender, EventArgs e) => LoadUserControl(new UCReservation());
+        private void sataButton3_Click(object sender, EventArgs e) => LoadUserControl(new UCRooms());
+        private void sataButton4_Click(object sender, EventArgs e) => LoadUserControl(new UCCustomers());
+        private void sataButton5_Click(object sender, EventArgs e) => LoadUserControl(new UCBilling());
+        private void sataButton6_Click(object sender, EventArgs e) => LoadUserControl(new UCSettings());
+        private void sataButton8_Click(object sender, EventArgs e) => LoadUserControl(new UCDashboard());
+    }
+}
---
+++ HotelReservationSystem/HotelReservationSystem/UserControls/UCBilling.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace HotelReservationSystem.UserControls
+{
+    partial class UCBilling
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCBilling));
+            this.TopHomePanel = new System.Windows.Forms.Panel();
+            this.sataPictureBox1 = new SATAUiFramework.Controls.SATAPictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TopHomePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopHomePanel
+            // 
+            this.TopHomePanel.BackColor = System.Drawing.Color.White;
+            this.TopHomePanel.Controls.Add(this.sataPictureBox1);
+            this.TopHomePanel.Controls.Add(this.label3);
+            this.TopHomePanel.Controls.Add(this.label4);
+            this.TopHomePanel.Controls.Add(this.label5);
+            this.TopHomePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopHomePanel.Location = new System.Drawing.Point(0, 0);
+            this.TopHomePanel.Name = "TopHomePanel";
+            this.TopHomePanel.Size = new System.Drawing.Size(1477, 72);
+            this.TopHomePanel.TabIndex = 2;
+            // 
+            // sataPictureBox1
+            // 
+            this.sataPictureBox1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.sataPictureBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(79)))), ((int)(((byte)(165)))));
+            this.sataPictureBox1.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(68)))), ((int)(((byte)(142)))));
+            this.sataPictureBox1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.sataPictureBox1.BorderSize = 1;
+            this.sataPictureBox1.GradientAngle = 50F;
+            this.sataPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("sataPictureBox1.Image")));
+            this.sataPictureBox1.Location = new System.Drawing.Point(1060, 16);
+            this.sataPictureBox1.Name = "sataPictureBox1";
+            this.sataPictureBox1.Size = new System.Drawing.Size(43, 43);
+            this.sataPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sataPictureBox1.TabIndex = 2;
+            this.sataPictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(1111, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mark Manalo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(1113, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Admin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(22, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Billing";
+            // 
+            // UCBilling
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(248)))), ((int)(((byte)(250)))));
+            this.Controls.Add(this.TopHomePanel);
+            this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "UCBilling";
+            this.Size = new System.Drawing.Size(1477, 800);
+            this.TopHomePanel.ResumeLayout(false);
+            this.TopHomePanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.sataPictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopHomePanel;
+        private SATAUiFramework.Controls.SATAPictureBox sataPictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
